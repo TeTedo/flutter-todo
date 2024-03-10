da79fb88bacb394787bbfc86d0411e19779050bd
박태석 <qkrxotjr9508@gmail.com>
2024-03-10 16:23:39 +0900

    study : statefull widget

---
+++ practice_statefull/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: const TextTheme(
+              titleLarge: TextStyle(
+        color: Colors.red,
+      ))),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              showTitle ? MyLargeTitle() : Text('nothing'),
+              IconButton(
+                  onPressed: toggleTitle, icon: Icon(Icons.remove_red_eye)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    // build method 전에 한번만 호출됨
+    print("initState");
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // 위젯이 스크린에서 사라질때 호출
+    print("dispose");
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge!.color),
+    );
+  }
+}
